160e7c94a6c2ff28d48539d2297908a51dbb7829
William Vabrinskas <wvabrinskas@gmail.com>
2020-12-30 09:53:50 -0500

    added view

--- Sources/Graphy/Graphy.swift
+++ Sources/Graphy/Graphy.swift
@@ -1,3 +1,142 @@
-struct Graphy {
-    var text = "Hello, World!"
+import Foundation
+import UIKit
+
+public class Graphy: UIView {
+  private var points = [CGPoint]()
+  private var size: CGSize!
+  
+  public var scale:CGFloat = 100.0
+  public var pointSize = CGSize(width: 5.0, height: 5.0)
+
+  public init(points: [CGPoint], size: CGSize) {
+    super.init(frame: .zero)
+    
+    self.points = points
+    self.size = size
+    
+    let graphLayer = CALayer()
+    graphLayer.backgroundColor = UIColor.clear.cgColor
+    graphLayer.frame = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+    
+    let xOffset:CGFloat = 200.0
+    let yOffset:CGFloat = 200.0
+    
+    let maxHeight = self.size.height - yOffset
+    let maxWidth = self.size.width - xOffset
+    
+    let minX = xOffset / 2
+    let minY = yOffset / 2
+    let maxY = maxHeight + (yOffset / 2)
+    let maxX = maxWidth + (xOffset / 2)
+    
+    let ySpacing = maxHeight / 110
+    
+    var previousPoint: CGPoint?
+    
+    var x = 0
+    let line = CGMutablePath()
+    let lineLayer = CAShapeLayer()
+    let axisLineLayer = CAShapeLayer()
+    
+    let sorted = self.points.sorted(by: { $0.y < $1.y })
+    let lastXPoint = sorted.last!.x
+    
+    for x in stride(from: minX, through: maxX, by: self.scale) {
+      let currentX = (lastXPoint / maxWidth) * (x - minX)
+      
+      let graphlabel = UILabel(frame: CGRect(x: x - 10, y: minY - 25, width: 50, height: 20))
+      graphlabel.text = "\(Int(currentX))"
+      graphlabel.sizeToFit()
+      
+      self.addSubview(graphlabel)
+      
+      //scores
+      let scoreLayer = self.scoreLine(from: CGPoint(x: x, y: minY), to: CGPoint(x: x, y: maxY))
+      graphLayer.addSublayer(scoreLayer)
+    }
+    
+    for y in stride(from: minY, through: maxY, by: self.scale) {
+      let currentY = (110 / maxHeight) * (y - minY)
+      
+      let graphlabel = UILabel(frame: CGRect(x: minX - 30.0, y: y - 5, width: 50, height: 20))
+      graphlabel.text = "\(Int(currentY))"
+      graphlabel.sizeToFit()
+      
+      self.addSubview(graphlabel)
+      
+      let scoreYLayer = self.scoreLine(from: CGPoint(x: minX, y: y), to: CGPoint(x: maxX, y: y))
+      graphLayer.addSublayer(scoreYLayer)
+      
+    }
+    
+    for point in sorted {
+      
+      let currentX = (((point.x * maxWidth) / lastXPoint) + (xOffset / 2)) - (pointSize.width / 2)
+      let currentY = ((point.y * ySpacing) + (yOffset / 2)) - (pointSize.height / 2)
+      
+      let oval = CGPath(ellipseIn: CGRect(x:currentX, y: currentY, width: pointSize.width, height: pointSize.height), transform: nil)
+      let shapeLayer = CAShapeLayer()
+      
+      shapeLayer.fillColor = UIColor.red.cgColor
+      shapeLayer.strokeColor = UIColor.clear.cgColor
+      shapeLayer.path = oval
+      
+      graphLayer.addSublayer(shapeLayer)
+      
+      if previousPoint != nil {
+        line.move(to:  CGPoint(x: previousPoint!.x + 2.5, y: previousPoint!.y + 2.5))
+        line.addLine(to: CGPoint(x: currentX + 2.5, y: currentY + 2.5))
+      }
+      
+      let pointLabel = self.pointLabel(currentPoint: CGPoint(x: currentX, y: currentY), value: point)
+      self.addSubview(pointLabel)
+      
+      
+      previousPoint = CGPoint(x: currentX, y: currentY)
+      x += 1
+    }
+    
+    lineLayer.strokeColor = UIColor.red.cgColor
+    lineLayer.lineWidth = 2.0
+    lineLayer.path = line
+    lineLayer.lineCap = .round
+    
+    let axis = CGMutablePath()
+    axis.move(to: CGPoint(x: minX, y: minY))
+    axis.addLine(to: CGPoint(x: maxX, y: minY))
+    axis.move(to: CGPoint(x: minX, y: minY))
+    axis.addLine(to: CGPoint(x: minX, y: maxY))
+    
+    axisLineLayer.strokeColor = UIColor.lightGray.cgColor
+    axisLineLayer.lineWidth = 2.5
+    axisLineLayer.path = axis
+    
+    graphLayer.addSublayer(axisLineLayer)
+    graphLayer.addSublayer(lineLayer)
+    
+    self.layer.addSublayer(graphLayer)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func scoreLine(from: CGPoint, to: CGPoint) -> CAShapeLayer {
+    let scoreLine = CGMutablePath()
+    scoreLine.move(to: from)
+    scoreLine.addLine(to: to)
+    let scoreLayer = CAShapeLayer()
+    scoreLayer.strokeColor = UIColor.lightGray.cgColor
+    scoreLayer.lineWidth = 1.0
+    scoreLayer.path = scoreLine
+    return scoreLayer
+  }
+  
+  private func pointLabel(currentPoint: CGPoint, value: CGPoint) -> UILabel {
+    let graphlabel = UILabel(frame: CGRect(x: currentPoint.x - 10, y: currentPoint.y - 10, width: 50, height: 20))
+    graphlabel.text = "(\(Int(value.x)), \(Int(value.y))%)"
+    graphlabel.sizeToFit()
+    return graphlabel
+  }
+  
 }
--- Tests/GraphyTests/GraphyTests.swift
+++ Tests/GraphyTests/GraphyTests.swift
@@ -6,7 +6,7 @@ final class GraphyTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Graphy().text, "Hello, World!")
+        
     }
 
     static var allTests = [
