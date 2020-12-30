0b31f4936266ee05ab990db9718585e64c44d68b
William Vabrinskas <wvabrinskas@gmail.com>
2020-12-30 16:31:58 -0500

    zoom?

--- Sources/Graphy/Graphy.swift
+++ Sources/Graphy/Graphy.swift
@@ -106,7 +106,7 @@ public class Graphy: UIView {
       let showAxis = viewModel.showAxisLabels ?? false
       
       if showAxis {
-        let graphlabel = UILabel(frame: CGRect(x: (x - 8) * zoomX, y: minY + 15, width: 50, height: 20))
+        let graphlabel = UILabel(frame: CGRect(x: x - 8, y: minY + 15, width: 50, height: 20))
         
         let rounded = Float(round(currentX * 10) / 10)
 
@@ -138,7 +138,7 @@ public class Graphy: UIView {
       let showAxis = viewModel.showAxisLabels ?? false
 
       if showAxis {
-        let graphlabel = UILabel(frame: CGRect(x: minX - 40.0, y: ((minY - y) - 5) * zoomY, width: 50, height: 20))
+        let graphlabel = UILabel(frame: CGRect(x: minX - 40.0, y: (minY - y) - (5 * zoomY), width: 50, height: 20))
         let rounded = Float(round(currentY * 1000) / 1000)
 
         graphlabel.text = "\(rounded)"
