11535c1bdbd77b883dbce9c796e96dce7e251f44
William Vabrinskas <wvabrinskas@gmail.com>
2020-12-30 13:39:56 -0500

    working on scale

--- Sources/Graphy/Graphy.swift
+++ Sources/Graphy/Graphy.swift
@@ -127,13 +127,18 @@ public class Graphy: UIView {
     
     for point in self.points {
       
+      let scaleY = viewModel.axisScale?.y ?? 100.0
+      let scaleX = viewModel.axisScale?.x ?? 100.0
+
       let zoomY = viewModel.zoom?.y ?? 1
       let zoomX = viewModel.zoom?.x ?? 1
       let pointSize = viewModel.pointSize ?? CGSize(width: 5, height: 5)
       
-      let currentX = ((((point.x * CGFloat(zoomX)) * maxWidth) / lastXPoint) + (offsetX / 2)) - (pointSize.width / 2)
-      let currentY = minY - ((point.y * CGFloat(zoomY)) * ySpacing) - (pointSize.height / 2)
+      var currentX = ((((point.x * CGFloat(zoomX)) * maxWidth) / lastXPoint) + (offsetX / 2)) - (pointSize.width / 2)
+      var currentY = minY - ((point.y * CGFloat(zoomY)) * ySpacing) - (pointSize.height / 2)
       
+      currentX /= scaleX
+      currentY /= scaleY
       
       let oval = CGPath(ellipseIn: CGRect(x: currentX,
                                           y: currentY,
