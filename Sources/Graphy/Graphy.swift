2c386a2b2448bb4fb3b40be5f8a9d9911b0158d4
William Vabrinskas <wvabrinskas@gmail.com>
2020-12-30 15:55:55 -0500

    derivations

--- Sources/Graphy/Graphy.swift
+++ Sources/Graphy/Graphy.swift
@@ -153,8 +153,8 @@ public class Graphy: UIView {
     
     for point in self.points {
       
-      let zoomX = ((scale.x * 100) / 100)
-      let zoomY = ((scale.y * 100) / 100)
+      let zoomX = ((scale.x * 100) / 100) * derivations.x
+      let zoomY = ((scale.y * 100) / 100) * derivations.y
 
       let pointSize = viewModel.pointSize ?? CGSize(width: 5, height: 5)
       
